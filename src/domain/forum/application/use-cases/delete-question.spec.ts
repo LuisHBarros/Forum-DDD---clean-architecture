d6f4840a1424e2ef4eeeb9b918a5bc5b248864af
luishbarros <luishrbr@outlook.com>
2024-05-15 16:50:18 -0300

    delete-question-use-case

--- src/domain/forum/application/use-cases/delete-question.spec.ts
+++ src/domain/forum/application/use-cases/delete-question.spec.ts
@@ -8,18 +8,36 @@ let fakeQuestionsRepository: InMemoryQuestionsRepository;
 let sut: DeleteQuestion;
 
 describe("Delete Question", () => {
-  beforeEach(() => {
-    fakeQuestionsRepository = new InMemoryQuestionsRepository();
-    sut = new DeleteQuestion(fakeQuestionsRepository);
-  });
-  it("should delete a question", async () => {
-    const question = await makeQuestion({});
-    await fakeQuestionsRepository.create(question);
-    expect(await sut.execute({ id: question.id.toString() })).toBeUndefined();
-  });
-  it("should throw an error if question does not exist", async () => {
-    expect(sut.execute({ id: "non-existent-id" })).rejects.toThrow(
-      "Question not found"
-    );
-  });
+    beforeEach(() => {
+        fakeQuestionsRepository = new InMemoryQuestionsRepository();
+        sut = new DeleteQuestion(fakeQuestionsRepository);
+    });
+    it("should delete a question", async () => {
+        const question = await makeQuestion({});
+        await fakeQuestionsRepository.create(question);
+        expect(
+            await sut.execute({
+                question_id: question.id.toString(),
+                author_id: question.authorId.toString(),
+            })
+        ).toBeUndefined();
+    });
+    it("should throw an error if question does not exist", async () => {
+        expect(
+            sut.execute({
+                question_id: "non-existent-id",
+                author_id: "non-existent-id",
+            })
+        ).rejects.toThrow("Question not found");
+    });
+    it("should throw an error if user is not the author of the question tries to delete it", async () => {
+        const question = await makeQuestion({});
+        await fakeQuestionsRepository.create(question);
+        expect(
+            sut.execute({
+                question_id: question.id.toString(),
+                author_id: "non-existent-id",
+            })
+        ).rejects.toThrow("You are not allowed to delete this question");
+    });
 });
--- src/domain/forum/application/use-cases/delete-question.ts
+++ src/domain/forum/application/use-cases/delete-question.ts
@@ -1,18 +1,24 @@
 import { QuestionsRepository } from "../repositories/questions-repository";
 
 interface deleteQuestionData {
-  id: string;
+    question_id: string;
+    author_id: string;
 }
 
 export class DeleteQuestion {
-  constructor(private questionsRepository: QuestionsRepository) {}
+    constructor(private questionsRepository: QuestionsRepository) {}
 
-  async execute(data: deleteQuestionData) {
-    const question = await this.questionsRepository.findById(data.id);
-    if (!question) {
-      throw new Error("Question not found");
+    async execute(data: deleteQuestionData) {
+        const question = await this.questionsRepository.findById(
+            data.question_id
+        );
+        if (!question) {
+            throw new Error("Question not found");
+        }
+        if (question.authorId.toString() !== data.author_id) {
+            throw new Error("You are not allowed to delete this question");
+        }
+        await this.questionsRepository.delete(question);
+        return;
     }
-    await this.questionsRepository.delete(question);
-    return;
-  }
 }
